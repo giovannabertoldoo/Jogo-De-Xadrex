f5fedae41d2a95bcf76e357d5e02d4aee80f9664
Giovanna Bertoldo <giovannabertoldo81@gmail.com>
2026-06-25 13:30:39 -0300

    Update and rename JogoDeXadrex.java to src/principal/java/JogoDeXadrex.java

---
+++ src/principal/java/JogoDeXadrex.java
@@ -0,0 +1,29 @@
+ppublic class JogoDeXadrez implements Jogo {
+
+    @Override
+    public void iniciar() {
+
+        Tabuleiro tabuleiro = new Tabuleiro();
+
+        Jogador jogadorBranco = new JogadorHumano('b');
+        Jogador jogadorPreto = new JogadorSintetico('p');
+
+        while (!tabuleiro.acabouOJogo()) {
+
+            jogadorBranco.jogar(tabuleiro, "P1b", 1);
+            jogadorPreto.jogar(tabuleiro, "P1p", 1);
+
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        JogoDeXadrez partida = new JogoDeXadrez();
+
+        partida.iniciar();
+
+        System.out.println("Giovanna");
+    }
+}
+    
