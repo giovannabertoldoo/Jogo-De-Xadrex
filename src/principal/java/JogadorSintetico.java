0f8c1a9e4b2a06fbb457024b40110e21520c5d0c
Giovanna Bertoldo <giovannabertoldo81@gmail.com>
2026-06-25 13:33:03 -0300

    Refactor JogadorSintetico class methods
    Refactor JogadorSintetico class for improved formatting and clarity.

---
+++ src/principal/java/JogadorSintetico.java
@@ -0,0 +1,14 @@
+public class JogadorSintetico extends Jogador {
+
+    public JogadorSintetico(char cor) {
+        super(cor);
+    }
+
+    @Override
+    public boolean jogar(Tabuleiro tabuleiro, String nomeDaPeca, int numeroDaCasa) {
+
+        System.out.println("Jogador sintético realizou uma jogada.");
+
+        return true;
+    }
+}
