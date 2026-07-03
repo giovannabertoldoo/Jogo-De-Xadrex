0a09da6f29322e65a18d6841f6fa54013638fc8f
Giovanna Bertoldo <giovannabertoldo81@gmail.com>
2026-07-02 21:49:13 -0300

    Refactor JogoDeXadrezTest for JUnit 4
    Refactor chess game tests to use JUnit 4 assertions and update test methods for clarity.

--- src/teste/java/JogoDeXadrexTest.java
+++ src/teste/java/JogoDeXadrexTest.java
@@ -1,46 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-public class JogoDeXadrezTest {
+public class TabuleiroTest {
 
     @Test
-    public void testarTabuleiro() {
+    public void testaCasaLivre() {
 
         Tabuleiro tabuleiro = new Tabuleiro();
 
-        assertFalse(tabuleiro.acabouOJogo());
-    }
+        // Casa vazia
+        assertTrue(tabuleiro.casaLivre("E4"));
 
-    @Test
-    public void testarBispo() {
-
-        Bispo bispo = new Bispo("B1b");
-
-        assertEquals(1, bispo.mover());
-    }
-
-    @Test
-    public void testarRei() {
-
-        Rei rei = new Rei("K0b");
-
-        assertEquals(1, rei.mover());
-    }
-
-    @Test
-    public void testarTorre() {
-
-        Torre torre = new Torre("T1b");
-
-        assertEquals(1, torre.mover());
-    }
-
-    @Test
-    public void testarRainha() {
-
-        Rainha rainha = new Rainha("Q0b");
-
-        assertEquals(1, rainha.mover());
+        // Casa ocupada
+        assertTrue(!tabuleiro.casaLivre("A1"));
     }
 }
